b4816b1f8814e58daa84b11c2b466b1c6fa9196b
cyko1218 <cyko1218@gmail.com>
2024-07-25 00:24:32 +0900

    카카오톡 로그인 구현

--- app/src/main/java/com/example/meetteam/IntroActivity.kt
+++ app/src/main/java/com/example/meetteam/IntroActivity.kt
@@ -2,6 +2,7 @@ package com.example.meetteam
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.example.meetteam.databinding.ActivityIntroBinding
 
@@ -9,12 +10,16 @@ class IntroActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityIntroBinding
 
+    private val kakaoAuthViewModel: KakaoAuthViewModel by viewModels()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityIntroBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.kakaoLoginButton.setOnClickListener {
+            kakaoAuthViewModel.handleKakaoLogin()
             // 카카오 로그인 로직 추가
         }
 
