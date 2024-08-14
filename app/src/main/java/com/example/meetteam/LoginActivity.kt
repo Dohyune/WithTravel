77ba59f96f6b995ca6f59b19e9701b7b1eaa44b5
Dohyune <dhk2gd@gmail.com>
2024-08-14 12:27:19 +0900

    로그인화면 수정

--- app/src/main/java/com/example/meetteam/LoginActivity.kt
+++ app/src/main/java/com/example/meetteam/LoginActivity.kt
@@ -3,15 +3,20 @@ package com.example.meetteam
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.example.meetteam.databinding.ActivityLoginBinding
+import kotlinx.coroutines.launch
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
+
+    private val kakaoAuthViewModel: KakaoAuthViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +27,16 @@ class LoginActivity : AppCompatActivity() {
         val passwordEditText: EditText = binding.password
         val loginButton: Button = binding.loginButton
         val backButton: Button = binding.backButton
+        val kakaoLoginButton: Button = binding.kakaoLoginButton
+
+        // 카카오 로그인 상태 체크 및 처리
+        lifecycleScope.launch {
+            kakaoAuthViewModel.isLoggedIn.collect { isLoggedIn ->
+                if (isLoggedIn) {
+                    navigateToMainActivity()
+                }
+            }
+        }
 
         loginButton.setOnClickListener {
             val username = usernameEditText.text.toString()
@@ -37,12 +52,35 @@ class LoginActivity : AppCompatActivity() {
                 startActivity(intent)
                 finish()
             } else {
-                Toast.makeText(this@LoginActivity, "Invalid username or password", Toast.LENGTH_SHORT).show()
+                // 비밀번호가 틀린 경우
+                binding.wrongAlert.visibility = View.VISIBLE
             }
         }
+
+        usernameEditText.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                binding.wrongAlert.visibility = View.GONE
+            }
+        }
+        passwordEditText.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                binding.wrongAlert.visibility = View.GONE
+            }
+        }
+
+        // 카카오 로그인 버튼 클릭 리스너
+        kakaoLoginButton.setOnClickListener {
+            kakaoAuthViewModel.handleKakaoLogin()
+        }
+
         // 뒤로가기 버튼 클릭 리스너
         backButton.setOnClickListener {
             finish()  // 현재 액티비티를 종료하고 이전 액티비티로 돌아감
         }
+    }
+    private fun navigateToMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
